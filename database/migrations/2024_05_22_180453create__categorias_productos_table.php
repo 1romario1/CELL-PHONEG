4b907ccda35cc706e2b45a49b851ac16f201eddf
Romario Duglas Gonzales Montero <romgonmo@dmasterdorado.net>
2024-05-22 13:13:40 -0500

    primer commit

---
+++ database/migrations/2024_05_22_180453create__categorias_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_productos', function (Blueprint $table) {
+            $table->id('Id_Categoria');
+            $table->string('Tipo_Categoria');
+            $table->string('Descripcion_Categoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_categorias_productos');
+    }
+};
---
+++ database/migrations/2024_06_22_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('Numero_Documento');
+            $table->string('Nombre');
+            $table->string('Apellido');
+            $table->string('Correo_Electronico')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('Contraseña');
+            $table->integer('Numero_Telefono');
+            $table->foreignID('Id_Rol');
+            $table->foreign('Id_Rol')->references('Id_Rol')->on('rol');
+            $table->string('Estado',length:20);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
